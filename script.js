23ea5496c397e26d5e27f20b08f5baf926cd247f
Brian Parker <btparker70@gmail.com>
2020-12-22 02:27:44 -0800

    added on-click function for blocks. added functionality for number buttons that enter numbers onto game board

--- script.js
+++ script.js
@@ -22,20 +22,64 @@ var easyGame2 = [
     [6, null, null, null, 5, 4, null, 7, 1],
 ];
 
-gamePopulator(easyGame1);
+// Easy games
+var easyGames = [easyGame1, easyGame2];
+
+// Populates game board
 function gamePopulator(gameArray) {
     for (i = 0; i < gameArray.length; i++) {
         for (j = 0; j < gameArray[i].length; j++) {
-            console.log(gameArray[i][j])
             var letter = String.fromCharCode(65 + j);
             $(`#block_${letter}${i + 1}`).text(gameArray[i][j]);
-            console.log($(`#block_${letter}${j + 1}`))
         }
     }
 }
 
-
-$('#new-game').on('click', function() {
+// New-game button
+$('#new-game').on('click', function () {
     $('.block').empty();
-    gamePopulator(easyGame2);
+    var randomNum = Math.floor(Math.random() * easyGames.length);
+    gamePopulator(easyGames[randomNum]);
+})
+
+// Select empty block: Change block color when clicked
+$('.block').on('click', function () {
+    $('.block').css('background-color', 'white');
+    $(this).css('background-color', 'rgb(64, 187, 64)');
+})
+
+// Select empty block: Change row/col color when clicked
+$('.block').on('click', function () {
+    // If the block is empty
+    if ($(this).text() === '') {
+        // Letter
+        var row = $(this).attr('id').charAt(6);
+        // Number
+        var col = $(this).attr('id').charAt(7);
+        $('.block').css('background-color', 'white');
+
+        // Colors the row and column selected
+        for (i = 1; i < 10; i++) {
+            $(`#block_${row}${i}`).css('background-color', 'rgb(131, 207, 131)');
+        }
+        for (j = 0; j < 9; j++) {
+            var letter = String.fromCharCode(65 + j);
+            $(`#block_${letter}${col}`).css('background-color', 'rgb(131, 207, 131)');
+        }
+
+        // Colors the selected square
+        $(this).css('background-color', 'rgb(64, 187, 64)');
+    } else
+    // Color all same blocks with number
+    {
+        $('.block').css('background-color', 'white');
+        var num = $(this).text();
+        $(`.block:contains('${num}')`).css('background-color', 'rgb(64, 187, 64)');
+    }
+})
+
+// Enter number on board from button
+$('.clicker-block').on('click', function () {
+    const getNum = $(this).text();
+    $(".block[style='background-color: rgb(64, 187, 64);']").text(getNum);
 })
