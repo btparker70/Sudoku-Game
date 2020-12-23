c6757cb944fb379fe705e0b597aeb0d4f7aa1d59
Brian Parker <btparker70@gmail.com>
2020-12-22 23:28:33 -0800

    added 2 more easy puzzles

--- games/easygames.js
+++ games/easygames.js
@@ -20,5 +20,39 @@ var easyGames = [
         [null, 1, null, null, 3, 7, null, 4, null],
         [null, null, 5, null, null, null, null, null, 6],
         [6, null, null, null, 5, 4, null, 7, 1],
+    ],
+    [
+        [null, null, 9, null, 7, null, null, 8, null],
+        [6, 4, null, null, 1, 2, null, null, null],
+        [null, null, null, 5, 9, 4, 6, null, 1],
+        [8, null, 7, 1, null, null, 3, null, null],
+        [null, null, 2, 4, null, 6, 7, null, null],
+        [null, null, 6, null, null, 8, 2, null, 9],
+        [5, null, 8, 9, 6, 1, null, null, null],
+        [null, null, null, 2, 4, null, null, 9, 7],
+        [null, 2, null, null, 8, null, 1, null, null],
+    ],
+    [
+        [null, 1, 8, 2, null, 5, null, 4, null],
+        [null, null, 3, null, 8, null, null, null, null],
+        [7, 2, null, 4, null, 3, null, null, 1],
+        [null, 3, 6, null, null, null, null, 1, null],
+        [9, null, null, 6, null, 7, null, null, 4],
+        [null, 7, null, null, null, null, 5, 8, null],
+        [1, null, null, 3, null, 8, null, 6, 5],
+        [null, null, null, null, 7, null, 1, null, null],
+        [null, 6, null, 9, null, 4, 3, 7, null]
     ]
 ];
+
+// [
+//     [],
+//     [],
+//     [],
+//     [],
+//     [],
+//     [],
+//     [],
+//     [],
+//     [],
+// ]
